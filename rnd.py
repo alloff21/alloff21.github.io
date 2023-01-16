6ec7ee6273835660cdfab2bda71d7081a2a4578f
alloff21 <96178456+alloff21@users.noreply.github.com>
2023-01-16 17:39:51 +0300

    Update rnd.py

--- rnd.py
+++ rnd.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
+
+import cgi
 from random import randint
 
-def add_two_numbers(x, y):
-    return x + y
-
 def generate_random_number(x, y):
+    our_form = cgi.FieldStorage()
+    x = int(our_form.getfirst("x","0"))
+    y = int(our_form.getfirst("y","0"))
     z = randint(x, y)
     return z
